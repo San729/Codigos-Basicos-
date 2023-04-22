aa268bf30d4040a3b1854d4ca1a43b1e3548876f
sanderley santos <125043787+San729@users.noreply.github.com>
2023-04-22 10:08:29 -0300

    Create  Angulo Cartesiano.c
    Motivação
    Aline: Ei André, minha vida deu uma volta de 360 graus.
    André: Vai pro colégio não tu? 360 graus volta pro mesmo lugar.
    Aline: Eu sei, é porque eu achei na rua uma carteira sem nenhum documento e cheia de dinheiro na rua.
    André: Então é 180 graus, mulher, tu agora tá estribada.
    Aline: Não, é 360 mesmo, eu levei a carteira pra casa.
    André: E?????
    Aline: Descobri que era do meu pai.
    
    Ação
    
    
    Nosso sistema de ângulos no plano cartesiano é definido em graus. O ângulo 0 aponta para esquerda, o 90 aponta para cima, o 180 para direita e por aí vai. O 360 graus equivale voltar ao 0.
    
    Seu objetivo é transformar os ângulos para recolocá-los dentro do cartesiano. O ângulo 361 equivale ao 1. Ângulos negativos equivalem a andar no sentido horário. Por exemplo, -1 equivale ao 359.
    
    Você deve ler o ângulo e imprimir o valor correto dele dentro do cartesiano entre 0 e 359.

---
+++ Angulo Cartesiano.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int angulo;
+    scanf("%d", &angulo);
+    angulo = angulo % 360;
+    if (angulo < 0) {
+        angulo = angulo + 360;
+    }
+    printf("%d\n", angulo);
+    return 0;
+}
