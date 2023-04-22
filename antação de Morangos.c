0f7fe76c86f7bcc6035c65938dfdc06d015142db
sanderley santos <125043787+San729@users.noreply.github.com>
2023-04-22 10:14:52 -0300

    Create antação de Morangos.c
    Motivação
    Os administradores da Fazenda Fartura planejam criar uma nova plantação de morangos, no formato retangular. Eles têm vários locais possíveis para a nova plantação, com diferentes dimensões de comprimento e largura. Para os administradores, o melhor local é aquele que tem a maior área. Eles gostariam de ter um programa de computador que, dadas as dimensões de dois locais, determina o que tem maior área. Você pode ajudá-los?
    
    Entrada
    A entrada contém quatro linhas, cada uma contendo um número inteiro. As duas primeiras linhas indicam as dimensões (comprimento e largura) de um dos possíveis locais. As duas últimas linhas indicam as dimensões (comprimento e largura) de um outro possível local para a plantação de morangos. As dimensões são dadas em metros.
    Saída
    Seu programa deve escrever uma linha contendo um único inteiro, a área, em metros quadrados, do melhor local para a plantação, entre os dois locais dados na entrada.

---
+++ antação de Morangos.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    
+    int L1,C1,L2,C2,a1,a2;
+    
+    
+   scanf("%d %d %d %d",&C1,&L1,&C2,&L2);
+   
+   a1= L1*C1;
+   
+   a2=L2*C2;
+   
+   if(a1>a2){printf("%d\n",a1);}
+   
+   else{printf("%d\n",a2);}
+   
+   
+}
